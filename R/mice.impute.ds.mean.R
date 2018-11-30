64839c6b720295bcf81304983da9a7a5758972d0
Stef van Buuren <stef.vanbuuren@tno.nl>
2018-11-30 15:09:42 +0100

    Create mice.impute.ds.mean() client

--- R/mice.impute.ds.mean.R
+++ R/mice.impute.ds.mean.R
@@ -3,11 +3,13 @@
 #'Imputes the arithmetic mean of the observed data
 #'
 #'@inheritParams mice.impute.ds.pmm
-#'@return Vector with imputed data, same type as \code{y}, and of length 
+#'@param datasources A list with cluster information, usally the result
+#'of a call to \code{opal::datashield.login()}.
+#'@return Vector with imputed data, same type as \code{y}, and of length
 #'\code{sum(wy)}
 #'@section Warning: Imputing the mean of a variable is almost never
-#'appropriate.  See Little and Rubin (2002, p. 61-62) or 
-#'Van Buuren (2012, p. 10-11) 
+#'appropriate.  See Little and Rubin (2002, p. 61-62) or
+#'Van Buuren (2012, p. 10-11)
 #'@seealso \code{\link{mice}}, \code{\link{mean}}
 #'@references Van Buuren, S., Groothuis-Oudshoorn, K. (2011). \code{mice}:
 #'Multivariate Imputation by Chained Equations in \code{R}. \emph{Journal of
@@ -17,15 +19,21 @@
 #'Little, R.J.A. and Rubin, D.B. (2002). Statistical Analysis with Missing
 #'Data.  New York: John Wiley and Sons.
 #'
-#'Van Buuren, S. (2018). 
+#'Van Buuren, S. (2018).
 #'\href{https://stefvanbuuren.name/fimd/sec-simplesolutions.html#sec:meanimp}{\emph{Flexible Imputation of Missing Data. Second Edition.}}
 #'Chapman & Hall/CRC. Boca Raton, FL.
 #'@family univariate imputation functions
 #'@keywords datagen
 #'@export
-mice.impute.ds.mean <- function(y, ry, x = NULL, wy = NULL, ...)
+mice.impute.ds.mean <- function(y, ry, x = NULL, wy = NULL,
+                                datasources = NULL, ...)
 {
+  if (is.null(datasources))
+    return(mice.impute.mean(y, ry, x, wy, ...))
   if (is.null(wy))
     wy <- !ry
-  return(rep.int(mean(y[ry]), times = sum(wy)))
+  x <- paste("D", get("j", envir = parent.frame()), sep = "$")
+  my <- ds.mean(x = x, datasources = datasources)
+  my <- as.numeric(my)
+  return(rep.int(my, times = sum(wy)))
 }
